8e550e8b6369fa7cd973dc14dce15a07a9b54350
pwnsauce403 <40787695+pwnsauce403@users.noreply.github.com>
2023-06-26 21:48:09 +0700

    ㅤ

--- ud64.php
+++ ud64.php
@@ -1,24 +1,60 @@
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<style>*{font-family:Arial;}a{text-decoration:none;}</style>
-<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/base-min.css">
+<title>UD64 Deobfuscator</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8" />
+<style>
+body {
+  font-size: 12px;
+  font-family: ubuntu mono;
+  background-color: #1e1e2e;
+  color: white;margin:0;}
+a {
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+}
+a:hover, a:active {
+  color: #868686;
+  text-decoration: underline;
+}
+.button {
+  background-color: #1e1e2e;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  display: inline-block;
+  cursor: pointer;
+}
+.peli {padding:5px;width:100px;margin-top:10px;border-radius:4px;}
+</style>
+</head>
 <body>
 <center>
-<?php
+<div style="font-size:16px; text-align:center; font-weight:bold;">
+<pre><a href="ud64.php" style="text-decoration:none;">UD64 Deobfuscator</a></pre>
+</div>
+<?=@null;
 if($_POST){
-$obfuscated_data = $_POST['obfuscated'];
-$cleaned_data = gzinflate(convert_uudecode(base64_decode(gzinflate(base64_decode(str_rot13(explode('"', explode('$uD64_c0m("',$obfuscated_data)[1])[0]))))));
-$cleaned_data = explode('header("Location: http://ud64.com/"); die(); } ', $cleaned_data)[1];
+$obfus = $_POST['obfuscated'];
+$deob = gzinflate(convert_uudecode(base64_decode(gzinflate(base64_decode(str_rot13(explode('"', explode('$uD64_c0m("',$obfus)[1])[0]))))));
+$deob = explode('header("Location: http://ud64.com/"); die(); } ', $deob)[1];
 ?>
 <textarea cols="60" rows="20" readonly>&lt;?php
-<?=$cleaned_data?></textarea>
-<?php
+<?=$deob?></textarea>
+<?=@null;
 }
 ?>
+<style type="text/css">
+input, textarea {
+border-radius:6px;
+}
+</style>
 <form method="post">
-<textarea name="obfuscated" placeholder="paste your obfuscate code here..." cols="60" rows="20" class="form-control"></textarea><br><br>
-<button type="submit">FIRE!</button>
+<textarea name="obfuscated" placeholder="paste your ud64 obfuscate code from http://ud64.com/ here..." cols="60" rows="20" class="form-control"></textarea><br><br>
+<button type="submit" class="peli">FIRE!</button>
 </form>
 </center>
-</body></html>
+</body>
+</html>
